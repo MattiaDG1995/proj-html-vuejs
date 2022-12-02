54c2e4d36fa1fcf529fc54fccf6d003fb7922ff9
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-12-02 17:53:50 +0100

    main: fine sect2

--- src/components/Sect2Main.vue
+++ src/components/Sect2Main.vue
@@ -2,18 +2,56 @@
     <div class="section">
         <div class="company">
             <div class="boxcont">
-                <div>
+                <div class="box1">
                     <h5>ABOUT THE NETWORK</h5>
                 </div>
 
-                <div>
-                    <h2>The Company</h2>
+                <div class="box2">
+                    <span>The</span>
+                    <span>Company</span>
                 </div>
 
-                <div>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt accusantium, temporibus adipisci, voluptas accusamus</p>
+                <div class="box3">
+                    <p class="grey">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt accusantium, temporibus adipisci, voluptas accusamus</p>
                 </div>
+
+                <div class="contperc">
+                    <div class="boxperc">
+                        <div class="numb">
+                            <h2 class="percent">0%</h2>
+                        </div>
+                        <div>
+                            <h2 class="leader">Leadership</h2>
+                            <p class="grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                    
+                    <div class="boxperc">
+                        <div class="numb">
+                            <h2 class="percent">0%</h2>
+                        </div>
+                        <div>
+                            <h2 class="leader">Psychology</h2>
+                            <p class="grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>   
+
+                    <div class="boxperc">
+                        <div class="numb">
+                            <h2 class="percent">1%</h2>
+                        </div>
+                        <div>
+                            <h2 class="leader">Flexibility</h2>
+                            <p class="grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>   
+                </div>
+
             </div>
+        </div>
+
+        <div class="contimg">
+            <img src="../assets/img/about-5.jpg" alt="">
         </div>
     </div>
 </template>
@@ -25,8 +63,66 @@
 </script>
 
 <style lang="scss" scoped>
+
+    .section{
+        display: flex;
+    }
     .company{
         background-color: #111117;
         color: white;
+    }
+
+    .boxcont{
+        width: 70%;
+        margin: 0 auto;
+        margin-top: 250px;
+    }
+
+    .box1, .box2, .box3{
+        padding: 10px;
+    }
+
+    .box1{
+        color: #058283;
+    }
+
+    .box2{
+        font-size: 3rem;
+        span:first-child{
+            background: rgba(5, 130, 131, 0.3);
+            padding: 5px 8px;
+        }
+    }
+
+    .contperc{
+        margin-top: 50px;
+    }
+    .boxperc{
+        display: flex;
+        padding: 30px 0px;
+    }
+
+    .percent{
+        border: 8px solid grey;
+        border-radius: 50%;
+        padding: 15px;
+    }
+
+    .numb{
+        margin-right: 15px;
+    }
+
+    .leader{
+        margin-bottom: 20px;
+    }
+    .contimg{
+        img{         
+            width: 100%;
+            display: block;
+        }
+    }
+
+    .grey{
+        color: grey;
     }
 </style>
